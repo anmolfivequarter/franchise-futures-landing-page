af7a68466b2c0ab181be9e7ce2e32e353b55a110
Anmol Nandha <anmol@fivequarterdesigns.com>
2018-01-06 00:11:09 +0000

    Initial progress

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.typekit.net/nrx1onf.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>Franchise Futures</title>
+</head>
+
+<body>
+	<header>
+		<div class="container">
+			<div class="d-flex justify-content-center">
+				<img src="img/franchise-futures-logo@2x.png" alt="Franchise Futures - Helping you move forward">
+			</div>
+		</div>
+	</header>
+	<section class="content">
+		<div class="container">
+			<h1 class="text-center">Website coming soon...</h1>
+			<h2 class="text-center">In the meantime, if you’d like to get in touch<br> please submit an enquiry using the form below</h2>
+		</div>
+	</section>
+	<section class="enquiry">
+		<div class="container d-flex justify-content-center">
+			<div class="enquiry-form">
+				<form>
+					<div class="form-group">
+						<label for="txtFullName" class="sr-only">Full Name</label>
+						<input type="text" class="form-control" id="txtFullName" aria-describedby="fullNameHelp" placeholder="Full Name">
+					</div>
+					<div class="form-group">
+						<label for="txtEmail" class="sr-only">Email address</label>
+						<input type="email" class="form-control" id="txtEmail" aria-describedby="emailHelp" placeholder="Enter email">
+					</div>
+					<div class="form-group">
+						<label for="txtTelNo" class="sr-only">Telephone Number</label>
+						<input type="tel" class="form-control" id="txtTelNo" placeholder="Telephone Number">
+					</div>
+					<div class="form-group">
+						<select name="ddlIAm" id="ddlIAm" class="form-control">
+							<option value="">I am...</option>
+							<option value="">A franchisor</option>
+							<option value="">A franchisee</option>
+							<option value="">Looking for a franchise opportunity</option>
+							<option value="">Looking to convert my business into a franchise model</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="txtAdditionalInfo" class="sr-only">Additional Info</label>
+						<textarea name="txtAdditionalInfo" id="txtAdditionalInfo" class="form-control" placeholder="Additional Information"></textarea>
+					</div>
+					<div class="form-group">
+						<span class="custom-dropdown">
+							<select name="ddlBestTime" id="ddlBestTime" class="form-control">
+								<option value="">Best time to call...</option>
+								<option value="">Morning</option>
+								<option value="">Afternoon</option>
+								<option value="">Evening</option>
+								<option value="">Anytime</option>
+							</select>
+						</span>
+					</div>
+					<div class="form-group">
+						<label for="txtReferal" class="sr-only">Full Name</label>
+						<input type="text" class="form-control" id="txtReferal" aria-describedby="fullNameHelp" placeholder="Where did you hear of us?">
+					</div>
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+			</div>
+		</div>
+	</section>
+
+	<!-- Javascript -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+</body>
+
+</html>
